a23b2f58f01265c38735ea7608a4ffdbfc2fe520
Santiago Puerta <puertasantiago2002@gmail.com>
2024-04-01 00:42:20 -0500

    feat(adde code base): Added code base for aku.bin data format reader

---
+++ TelemedicineDataFormat/formatReader/src/dot_med_reader.py
@@ -0,0 +1,10 @@
+class DotMedReader():
+    def __init__(self): ...
+    
+    
+    def _get_files_in_folder():
+        # Get all .med files in given folder
+        ...
+    
+    def _read_files(): ...
+    
